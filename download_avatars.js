0a04de96016d372d3f3e6ccf7ee09cad215f17f4
M-Khoury <mkhoury87@hotmail.com>
2018-06-11 21:20:10 +0000

    fixed authorization token

--- download_avatars.js
+++ download_avatars.js
@@ -4,20 +4,28 @@ var token = require('./secrets.js');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  function getRepoContributors(repoOwner, repoName, cb) {
+  var contributors = [];
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': "token " + token
+      'Authorization': "token " + secrets.GITHUB_TOKEN
     }
   };
 
   request(options, function(err, res, body) {
     cb(err, body);
   });
-}
-}
+
+    contributors = JSON.parse();
+
+    cb(contributors) {
+      for (i = 0; i < contributors.length; i ++) {
+        console.log(contributors[i].avatar_url);
+      }
+    }
+
+  }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
