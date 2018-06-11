921a1d048cefe20fa6d7786d4e89de3608c4a93d
M-Khoury <mkhoury87@hotmail.com>
2018-06-11 22:56:25 +0000

    step 8 - downloaded avatars

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,12 @@
 var request = require('request');
 var tokens = require('./secrets.js');
+var fs = require('fs');
+var contributors = [];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    var contributors = [];
+
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
@@ -22,6 +24,15 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
     for (i = 0; i < result.length; i++) {
-        console.log(result[i].avatar_url);
+      downloadImageByURL(result[i].avatar_url, result[i].login)
+      console.log(result[i].avatar_url);
     }
 });
+
+function downloadImageByURL(url, filePath) {
+    filePath = "./avatars/" + filePath + ".jpg"
+    request(url).pipe(fs.createWriteStream(filePath));
+  };
+
+
+
