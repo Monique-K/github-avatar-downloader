04650b5f032f7cba2ae7a42a6fd66215ec8dbccf
M-Khoury <mkhoury87@hotmail.com>
2018-06-11 23:28:47 +0000

    accepts CL arguments that are mandatory - step 10

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,12 @@ var request = require('request');
 var tokens = require('./secrets.js');
 var fs = require('fs');
 var contributors = [];
+owner = process.argv[2];
+name = process.argv[3];
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
@@ -20,14 +21,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
     });
 };
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+
+if (owner === undefined || name === undefined) {
+  console.log("Please try again by entering a repo owner and repo name!")
+} else {
+  getRepoContributors(owner, name, function(err, result) {
     console.log("Errors:", err);
     console.log("Result:", result);
     for (i = 0; i < result.length; i++) {
       downloadImageByURL(result[i].avatar_url, result[i].login)
       console.log(result[i].avatar_url);
     }
-});
+ });
+}
 
 function downloadImageByURL(url, filePath) {
     filePath = "./avatars/" + filePath + ".jpg"
